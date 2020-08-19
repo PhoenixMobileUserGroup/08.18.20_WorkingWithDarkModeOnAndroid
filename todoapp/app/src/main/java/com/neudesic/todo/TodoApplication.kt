51fcc9a21120eab0deb294b46b7bc232e64031d3
Rachelle Boyette <rachelle.boyette@neudesic.com>
2020-08-18 18:53:41 -0700

    Final update on presentation

--- todoapp/app/src/main/java/com/neudesic/todo/TodoApplication.kt
+++ todoapp/app/src/main/java/com/neudesic/todo/TodoApplication.kt
@@ -1,4 +1,21 @@
 package com.neudesic.todo
 
-class TodoApplication {
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.PreferenceManager
+
+class TodoApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val theme = preferences.getString("theme", "Auto")
+
+        when (theme) {
+            "Light" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            "Dark" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            "Auto" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            "Battery Saver" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+        }
+    }
 }
